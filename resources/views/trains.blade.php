6b389130eb933c8f73d4b12e9f67f790f06e26cf
matteotagliatti <matteotagliatti@gmail.com>
2022-05-13 16:06:20 +0200

    display trains frontend

--- resources/views/trains.blade.php
+++ resources/views/trains.blade.php
@@ -1,17 +1,31 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel Migration Seeder</title>
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 
 <body>
-    <div>
-        <h1>Treni che partono oggi:</h1>
-        @dump($trains)
+    <h1>Treni che partono oggi:</h1>
+    <div class="grid">
+        @foreach ($trains as $train)
+            <div class="train">
+                <p>{{ $train->id }}</p>
+                <p>{{ $train->azienda }}</p>
+                <p>Stazione partenza: {{ $train->stazione_partenza }}</p>
+                <p>Stazione arrivo: {{ $train->stazione_arrivo }}</p>
+                <p>Giorno di partenza: {{ $train->giorno_partenza }}</p>
+                <p>Giorno di arrivo: {{ $train->giorno_arrivo }}</p>
+                <p>Orario di partenza: {{ $train->orario_partenza }}</p>
+                <p>Orario di arrivo: {{ $train->orario_arrivo }}</p>
+                <p>Cod treno: {{ $train->codice_treno }}</p>
+                <p>Numero carrozze: {{ $train->numero_carrozze }}</p>
+            </div>
+        @endforeach
     </div>
 </body>
 
