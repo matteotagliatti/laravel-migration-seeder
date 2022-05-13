fe97a79f14999d1ec34a90704c95a49c7625368d
matteotagliatti <matteotagliatti@gmail.com>
2022-05-13 15:57:19 +0200

    filter trains and dump on frontend

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Train;
 
 class TrainController extends Controller
 {
-    //
+    public function index()
+    {
+        $trains = Train::where([
+            ["giorno_partenza", '=', date('Y-m-d')],
+            ["cancellato", "=", 0]
+        ])->get();
+        $data = ["trains" => $trains];
+        return view('trains', $data);
+    }
 }
